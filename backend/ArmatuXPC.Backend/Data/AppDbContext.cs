9d80ffebc0216960a6c05bab2fa05efb01d99882
OscarDs7 <a22110112@ceti.mx>
2026-01-10 23:49:28 -0600

    Actualización de relaciones con FK de Armado.cs con Componente.cs

--- backend/ArmatuXPC.Backend/Data/AppDbContext.cs
+++ backend/ArmatuXPC.Backend/Data/AppDbContext.cs
@@ -12,5 +12,51 @@ namespace ArmatuXPC.Backend.Data
         public DbSet<Componente> Componentes { get; set; }
         public DbSet<Armado> Armados { get; set; }
         public DbSet<Compatibilidad> Compatibilidades { get; set; } 
+
+        // Configuración de las relaciones entre entidades o tablas
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Armado>(entity =>
+            {
+                entity.HasOne(a => a.Gabinete)
+                    .WithMany()
+                    .HasForeignKey(a => a.GabineteId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(a => a.PlacaBase)
+                    .WithMany()
+                    .HasForeignKey(a => a.PlacaBaseId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(a => a.FuentePoder)
+                    .WithMany()
+                    .HasForeignKey(a => a.FuentePoderId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(a => a.MemoriaRam)
+                    .WithMany()
+                    .HasForeignKey(a => a.MemoriaRamId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(a => a.Procesador)
+                    .WithMany()
+                    .HasForeignKey(a => a.ProcesadorId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(a => a.Almacenamiento)
+                    .WithMany()
+                    .HasForeignKey(a => a.AlmacenamientoId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(a => a.GPU)
+                    .WithMany()
+                    .HasForeignKey(a => a.GPUId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+
+
     }
 }
