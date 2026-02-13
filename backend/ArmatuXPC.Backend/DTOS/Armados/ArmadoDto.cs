7688606a316715b67b65c46d1365c03006b80515
OscarDs7 <a22110112@ceti.mx>
2026-02-12 22:52:34 -0600

    Nuevo archivo para enviar solo el objeto necesario al server en ASP.NET y al cliente en React

---
+++ backend/ArmatuXPC.Backend/DTOS/Armados/ArmadoDto.cs
@@ -0,0 +1,21 @@
+namespace ArmatuXPC.Backend.DTOs;
+using ArmatuXPC.Backend.Models;
+
+public class ArmadoDto
+{
+    public int ArmadoId { get; set; }
+    public required string NombreArmado { get; set; }
+
+    public required ComponentesDto Componentes { get; set; }
+}
+
+public class ComponentesDto
+{
+    public Componente? Procesador { get; set; }
+    public Componente? PlacaBase { get; set; }
+    public Componente? GPU { get; set; }
+    public Componente? MemoriaRam { get; set; }
+    public Componente? Almacenamiento { get; set; }
+    public Componente? FuentePoder { get; set; }
+    public Componente? Gabinete { get; set; }
+}
