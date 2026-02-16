17e889bf9a3f9a7cea3e322620501eea29e9716c
OscarDs7 <a22110112@ceti.mx>
2026-02-16 17:59:25 -0600

    Nuevo campo Precio en entidad Componente.cs

--- backend/ArmatuXPC.Backend/Models/Componente.cs
+++ backend/ArmatuXPC.Backend/Models/Componente.cs
@@ -1,4 +1,6 @@
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace ArmatuXPC.Backend.Models
 {
@@ -9,6 +11,9 @@ namespace ArmatuXPC.Backend.Models
         public string Nombre { get; set; } = string.Empty;
         public string Marca { get; set; } = string.Empty;
         public string Modelo { get; set; } = string.Empty;
+        
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Precio { get; set; } // Precio por individual
         public TipoComponente Tipo { get; set; } // 1: Procesador, 2: Tarjeta Gráfica (GPU), 3: Memoria RAM, 4: Almacenamiento, 5: Fuente de Poder, 6: Placa Base, 7: Gabinete
         public decimal Voltaje { get; set; }
 
