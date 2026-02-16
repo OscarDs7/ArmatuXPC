16838a27395f526ebf16d9cf469ae296c8420f8c
OscarDs7 <a22110112@ceti.mx>
2026-02-16 17:58:23 -0600

    Nueva migración para modificación en tabla Componentes - nuevo campo llamado Precio

---
+++ backend/ArmatuXPC.Backend/Migrations/20260216230826_AddPrecioToComponente.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArmatuXPC.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrecioToComponente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Precio",
+                table: "Componentes",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Componentes");
+        }
+    }
+}
