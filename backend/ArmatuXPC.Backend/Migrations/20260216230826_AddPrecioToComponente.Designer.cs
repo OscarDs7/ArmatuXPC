caaf7ad690bcc96ce1ebcbe5d6e6d643aec5c05f
OscarDs7 <a22110112@ceti.mx>
2026-02-16 17:58:23 -0600

    Nueva migración para modificación en tabla Componentes - nuevo campo llamado Precio

--- backend/ArmatuXPC.Backend/Migrations/20260216230826_AddPrecioToComponente.Designer.cs
+++ backend/ArmatuXPC.Backend/Migrations/20260216230826_AddPrecioToComponente.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ArmatuXPC.Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ArmatuXPC.Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260216230826_AddPrecioToComponente")]
+    partial class AddPrecioToComponente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
