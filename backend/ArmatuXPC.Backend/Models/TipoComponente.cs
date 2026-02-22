691af9d5c5df40363ad3be7a25e826e89f73ac5d
OscarDs7 <a22110112@ceti.mx>
2026-02-21 19:12:25 -0600

    Buena práctica de entendimiento de código

--- backend/ArmatuXPC.Backend/Models/TipoComponente.cs
+++ backend/ArmatuXPC.Backend/Models/TipoComponente.cs
@@ -2,8 +2,8 @@ using System.Text.Json.Serialization; // For JsonStringEnumConverter
 
 namespace ArmatuXPC.Backend.Models
 {
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public enum TipoComponente
+    [JsonConverter(typeof(JsonStringEnumConverter))] // Convierte el enum a string en JSON para una mejor legibilidad
+    public enum TipoComponente // Enum para representar los tipos de componentes de PC
     {
         CPU = 1,
         GPU = 2,
