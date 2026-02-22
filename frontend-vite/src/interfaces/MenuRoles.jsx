eafa07d5f1a371a59f205e5b3fb9c5b7bd02b2a8
OscarDs7 <a22110112@ceti.mx>
2026-02-22 14:11:10 -0600

    Instalación y configuración TailwindCSS (método híbrido)

--- frontend-vite/src/interfaces/MenuRoles.jsx
+++ frontend-vite/src/interfaces/MenuRoles.jsx
@@ -1,44 +1,36 @@
 // src/interfaces/MenuRoles.jsx
-import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../estilos/MenuRoles.css"; // archivo de estilos externos
-import logoProyecto from "../assets/Logo.png"; // imagen del logo del proyecto
-import fondoProyecto from "../assets/fondo1.jpg"; // imagen de fondo del proyecto
+import "../estilos/MenuRoles.css";
+import logoProyecto from "../assets/Logo.png";
+import fondoProyecto from "../assets/fondo1.jpg";
 
-// Componente MenuRoles
-// Este componente muestra un menú con dos botones para navegar a las páginas de usuario y administrador
-// Utiliza React Router para la navegación y estilos personalizados
 export function MenuRoles() {
   const navigate = useNavigate();
 
-  const goUsuario = () => {
-    navigate("/login-usuario");
-  };
-
-  const goAdmin = () => {
-    navigate("/login-admin");
-  };
-
   return (
-      <div  className="menu-container" style={{ backgroundImage: `url(${fondoProyecto})`, }} >
+    <div
+      className="menu-container"
+      style={{ backgroundImage: `url(${fondoProyecto})` }}
+    >
       <div className="menu-card">
-
         <h1 className="menu-title">ArmatuXPC</h1>
 
-        {/* LOGO DEL PROYECTO */}
-        <img src={logoProyecto} alt="ArmatuXPC logo" className="menu-logo" />
+       <div className="menu-logo-container">
+          <img src={logoProyecto} alt="ArmatuXPC logo" className="menu-logo" />
+       </div>
 
-        <button className="menu-btn" onClick={goUsuario}>
-          Usuario
-        </button>
+        <div className="menu-buttons">
+          <button className="menu-btn" onClick={() => navigate("/login-usuario")}>
+            Usuario
+          </button>
 
-        <button className="menu-btn" onClick={goAdmin}>
-          Administrador
-        </button>
-
+          <button className="menu-btn" onClick={() => navigate("/login-admin")}>
+            Administrador
+          </button>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default MenuRoles;
