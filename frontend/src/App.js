9140a8c8084a34e496f677ba32963927081159f2
OscarDs7 <a22110112@ceti.mx>
2026-02-21 19:09:49 -0600

    Nuevas rutas de prueba de backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import LoginUsuario from "./interfaces/LoginUser";
 import LoginAdmin from "./interfaces/LoginAdmin";
 import DashBoardAdmin from "./interfaces/DashBoardAdmin";
 import DashBoardUser from "./interfaces/DashBoardUser";
+import TestBackendBasico from "./interfaces/TestBackendBasico";
+import TestBackendMedio from "./interfaces/TestBackendMedio";
+import TestBackendCompleto from "./interfaces/TestBackendCompleto";
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
       <Route path="/roles" element={<MenuRoles />} />
       <Route path="/dash-admin" element={<DashBoardAdmin />} />
       <Route path="/dash-user" element={<DashBoardUser />} />
+      <Route path="/test-backend-basico" element={<TestBackendBasico />} />
+      <Route path="/test-backend-medio" element={<TestBackendMedio />} />
+      <Route path="/test-backend-completo" element={<TestBackendCompleto />} />
     </Routes>
   );
 }
