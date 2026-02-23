8d6025992c8591bd93c31243cb33790c618afce6
OscarDs7 <a22110112@ceti.mx>
2026-02-23 14:03:19 -0600

    Implementación de seguridad de rutas y brincos ilegales

--- frontend-vite/components/AdminRoute.jsx
+++ frontend-vite/components/AdminRoute.jsx
@@ -2,6 +2,7 @@
 
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import AdminRoute from "./AdminRoute";
 
 export default function AdminRoute({ children }) {
   const { user, rol } = useAuth();
