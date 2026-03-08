e419345a0d57ad7703af613046e3337c386066a7
OscarDs7 <a22110112@ceti.mx>
2026-03-08 00:00:27 -0600

    Agregación del campo ImagenUrl dentro de entidad Componente.cs

--- backend/ArmatuXPC.Backend/Models/Componente.cs
+++ backend/ArmatuXPC.Backend/Models/Componente.cs
@@ -25,7 +25,7 @@ namespace ArmatuXPC.Backend.Models
         public int? CapacidadWatts { get; set; } // Potencia total en watts que puede suministrar la fuente de poder, para validar que sea suficiente para el armado
         
         // Imagen del componente (URL o base64), para mostrar en el frontend
-        public string? Imagen { get; set; }
+        public string? ImagenUrl { get; set; }
 
 
 
