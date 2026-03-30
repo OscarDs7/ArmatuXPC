129ad82abd416011f7c61b2eed2e574edc64f166
OscarDs7 <a22110112@ceti.mx>
2026-03-29 22:01:11 -0600

    Nuevas migraciones agregando nuevos campos: EsPublicado y AutorNombre

---
+++ backend/ArmatuXPC.Backend/Migrations/20260330000942_AddNombreAutorArmado.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArmatuXPC.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNombreAutorArmado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AutorNombre",
+                table: "Armados",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutorNombre",
+                table: "Armados");
+        }
+    }
+}
