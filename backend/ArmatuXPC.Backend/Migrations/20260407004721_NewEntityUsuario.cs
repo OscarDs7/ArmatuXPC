7c116ea72193fb4c919859a8ab318be619ae3868
OscarDs7 <a22110112@ceti.mx>
2026-04-06 20:27:32 -0600

    Nueva migración con nueva entidad Usuario.cs dentro de la BD

---
+++ backend/ArmatuXPC.Backend/Migrations/20260407004721_NewEntityUsuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArmatuXPC.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewEntityUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Uid = table.Column<string>(type: "text", nullable: false),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Correo = table.Column<string>(type: "text", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    TokensDisponibles = table.Column<int>(type: "integer", nullable: false),
+                    Rol = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Uid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
