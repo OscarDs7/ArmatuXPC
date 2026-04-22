e52265ea2f78ce087cf5321b19dfa430e652edf3
OscarDs7 <a22110112@ceti.mx>
2026-04-22 17:34:05 -0600

    Nueva columna booleana para saber si el usuario en su primer armado necesitó ayuda externa o pudo por su cuenta mediante el sistema inteligente de la plataforma

--- backend/ArmatuXPC.Backend/Models/FeedbackUsuario.cs
+++ backend/ArmatuXPC.Backend/Models/FeedbackUsuario.cs
@@ -7,4 +7,5 @@ public class FeedbackUsuario
     public string TipoHito { get; set; } = string.Empty; // "PRIMER_ARMADO" o "TERCER_ARMADO"
    // 💡 Cambio clave: Forzar DateTime.UtcNow para compatibilidad con Postgres
     public DateTime Fecha { get; set; } = DateTime.UtcNow;
+    public bool? CompletoSinAyuda { get; set; } // para conocer más a fondo experiencia de usuario en su primer armado
 }
