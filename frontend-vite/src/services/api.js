80b4d5179b773ea625ed9040eed67d4227eb70f6
OscarDs7 <a22110112@ceti.mx>
2026-05-05 00:55:15 -0600

    Nuevo método para conectar el backend con el frontend para el Chatbot

--- frontend-vite/src/services/api.js
+++ frontend-vite/src/services/api.js
@@ -543,4 +543,22 @@ export const eliminarFeedback = async (id) => {
   }
 };
 
+// --- MÉTODO PARA CHATBOT --- //
+// Método para enviar un mensaje al chatbot y obtener la respuesta del backend
+export const enviarMensajeChatbot = async (mensaje) => {
+  const response = await fetch(`${API_URL}/chatbot`, {  // 🔥 sin /api
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ mensaje })
+  });
+
+  if (!response.ok) {
+    throw new Error("Error al conectar con el chatbot");
+  }
+
+  return await response.json();
+};
+
+
+
 
