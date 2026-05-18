c3e848a4064b0fcc544e86c341add341a419251a
OscarDs7 <a22110112@ceti.mx>
2026-05-18 09:23:36 -0600

    Cambiar watts a int nullable en campos consumo y capacidad watts

---
+++ backend/ArmatuXPC.Backend/Migrations/20260518150207_ChangeWattsToInt.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArmatuXPC.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeWattsToInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ConsumoWatts",
+                table: "Componentes",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CapacidadWatts",
+                table: "Componentes",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ConsumoWatts",
+                table: "Componentes",
+                type: "numeric(10,2)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CapacidadWatts",
+                table: "Componentes",
+                type: "numeric(10,2)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
